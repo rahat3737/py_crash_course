46c53abaab9851e731d328de3fca7598fd9d9b23
rahat_rahman <146829873+rahat3737@users.noreply.github.com>
2024-09-15 14:09:57 +0600

    Add files via upload

---
+++ try_3.1.py
@@ -0,0 +1,5 @@
+name = ["sohan","mimman","mamun","maynul"]
+print(name[0].title())
+print(name[1].title())
+print(name[2].title())
+print(name[3].title())
---
+++ try_3.9.py
@@ -0,0 +1,2 @@
+guest_list = ["Imran","Shuvo","Arif","Roni","Hira"]
+print(f"I invite {len(guest_list)} people.")
