f175a995ca4374f8bd0266f226125e7972688bf0
rahat_rahman <146829873+rahat3737@users.noreply.github.com>
2024-09-15 14:19:59 +0600

    Delete try_3.2.py

--- try_3.2.py
+++
@@ -1,9 +0,0 @@
-name = ["sohan","mimman","mamun","maynul"]
-m1 = (f"Didn't your varsity opened yet, {name[0].title()} ?")
-print(m1)
-m2 = (f"Didn't your varsity opened yet, {name[1].title()} ?")
-print(m2)
-m3 = (f"Didn't your varsity opened yet, {name[2].title()} ?")
-print(m3)
-m4 = (f"Didn't your varsity opened yet, {name[3].title()} ?")
-print(m4)
